6b0705369e661ff362c957ed01adcf5051f48675
OVK <aminehifdi121@gmail.com>
2025-11-04 01:57:41 +0100

    sl

--- src/render_mv.c
+++ src/render_mv.c
@@ -1,113 +1,35 @@
 #include "../so_long.h"
 
-void	render_top(t_data *data)
+static void	move_player(t_data *d, int dx, int dy)
 {
-	int		pos_x;
-	int		pos_y;
+	int	x = d->pos.x / d->img.height;
+	int	y = d->pos.y / d->img.width;
+	char	next = d->map[y + dy][x + dx];
+	int		on_exit = (d->map[y][x] == d->content.exit);
 
-	pos_x = data->pos.x / data->img.height;
-	pos_y = data->pos.y / data->img.width;
-	if (data->map[pos_y - 1][pos_x] != data->content.wall)
+	if (next == d->content.wall)
+		return;
+	if (next == d->content.exit)
 	{
-		if (!(data->map[pos_y - 1][pos_x] == data->content.exit))
+		if (chk_collect(d) == 0)
 		{
-			data->map[pos_y - 1][pos_x] = data->content.player;
-			data->map[pos_y][pos_x] = data->content.space;
-			data->count += 1;
-			printf("count: %d\n", data->count);
+			printf("Félicitations ! Gagné en %d coups !\n", ++d->count);
+			end(d);
 		}
 		else
-		{
-			if (chk_collect(data) == 0)
-			{
-				printf("Felicitation vous avez gagne en : %d coup !\n",
-					data->count);
-				end(data);
-			}
-		}
+			d->map[y][x] = on_exit ? d->content.exit : d->content.space;
 	}
-}
-
-void	render_right(t_data *data)
-{
-	int		pos_x;
-	int		pos_y;
-
-	pos_x = data->pos.x / data->img.height;
-	pos_y = data->pos.y / data->img.width;
-	if (data->map[pos_y][pos_x + 1] != data->content.wall)
+	else
 	{
-		if (!(data->map[pos_y][pos_x + 1] == data->content.exit))
-		{
-			data->map[pos_y][pos_x + 1] = data->content.player;
-			data->map[pos_y][pos_x] = data->content.space;
-			data->count += 1;
-			printf("count: %d\n", data->count);
-		}
-		else
-		{
-			if (chk_collect(data) == 0)
-			{
-				printf("Felicitation vous avez gagne en : %d coup !\n",
-					data->count);
-				end(data);
-			}
-		}
+		d->map[y][x] = on_exit ? d->content.exit : d->content.space;
+		d->map[y + dy][x + dx] = d->content.player;
 	}
+	d->pos.x += dx * d->img.height;
+	d->pos.y += dy * d->img.width;
+	printf("count: %d\n", ++d->count);
 }
 
-void	render_left(t_data *data)
-{
-	int		pos_x;
-	int		pos_y;
-
-	pos_x = data->pos.x / data->img.height;
-	pos_y = data->pos.y / data->img.width;
-	if (data->map[pos_y][pos_x - 1] != data->content.wall)
-	{
-		if (!(data->map[pos_y][pos_x - 1] == data->content.exit))
-		{
-			data->map[pos_y][pos_x - 1] = data->content.player;
-			data->map[pos_y][pos_x] = data->content.space;
-			data->count += 1;
-			printf("count: %d\n", data->count);
-		}
-		else
-		{
-			if (chk_collect(data) == 0)
-			{
-				printf("Felicitation vous avez gagne en : %d coup !\n",
-					data->count);
-				end(data);
-			}
-		}
-	}
-}
-
-void	render_down(t_data *data)
-{
-	int		pos_x;
-	int		pos_y;
-
-	pos_x = data->pos.x / data->img.height;
-	pos_y = data->pos.y / data->img.width;
-	if (data->map[pos_y + 1][pos_x] != data->content.wall)
-	{
-		if (!(data->map[pos_y + 1][pos_x] == data->content.exit))
-		{
-			data->map[pos_y + 1][pos_x] = data->content.player;
-			data->map[pos_y][pos_x] = data->content.space;
-			data->count += 1;
-			printf("count: %d\n", data->count);
-		}
-		else
-		{
-			if (chk_collect(data) == 0)
-			{
-				printf("Felicitation vous avez gagne en : %d coup !\n",
-					data->count);
-				end(data);
-			}
-		}
-	}
-}
+void	render_top(t_data *d){move_player(d,0,-1);}
+void	render_right(t_data *d){move_player(d,1,0);}
+void	render_left(t_data *d){move_player(d,-1,0);}
+void	render_down(t_data *d){move_player(d,0,1);}
